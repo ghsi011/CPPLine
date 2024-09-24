5ae69c73418c24ce4b621b2fc93bba67f0fd26d3
ghsi011 <ghsi011@gmail.com>
2024-09-24 17:19:56 +0300

    wip

--- CPPLine/Main.cpp
+++ CPPLine/Main.cpp
@@ -1,7 +1,22 @@
 import std;
+import cppline;
 
-int main()
+constexpr cppline::Parser create_parser(const std::string& description) {
+    cppline::Parser parser(description);
+    parser.add_option<int>("-n", "Number", 10);
+    return parser;
+}
+
+int main(int argc, char* argv[])
 {
-    std::print("Hello, World!\n");
+    cppline::Parser parser = create_parser("This is a test parser");
+
+    std::vector<std::string> arguments;
+    for (int i = 1; i < argc; ++i) {
+        arguments.emplace_back(argv[i]);
+    }
+
+    parser.parse(arguments);
+    std::print("argument: {}", parser.get<int>("-n"));
     return 0;
 }
